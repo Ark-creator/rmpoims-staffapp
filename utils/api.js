24c7844d98daf249b296dff59b01d2033dfb6341
maze272003 <jg.jonatas.au@phinmaed.com>
2025-07-21 23:12:49 +0800

    location integrate

--- utils/api.js
+++ utils/api.js
@@ -4,18 +4,30 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
   baseURL: address,
-  timeout: 10000,
+  timeout: 10000, // Kept the timeout from your original code
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
 });
 
+// This interceptor adds the auth token to every request AFTER login
 api.interceptors.request.use(
   async (config) => {
+    // Get the token from storage
     const token = await AsyncStorage.getItem("authToken");
+
+    // If the token exists, add it to the Authorization header
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => {
+    // Handle request errors
+    return Promise.reject(error);
+  }
 );
 
 export default api;
